20bd7e4e8d8882be9be9381aedf51bb27001f872
wallmanderco <wallmanderco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-03 07:58:27 +0000

    Update
    git-svn-id: https://plugins.svn.wordpress.org/elasticsearch-indexer/trunk@1211625 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config/defaults.php
+++ config/defaults.php
@@ -9,14 +9,16 @@
  * file that was distributed with this source code.
  */
 
+use Wallmander\ElasticsearchIndexer\Model\Config;
+
 return [
     // System variables
-    'plugin_index_version'     => 3, // Update this value to prompt the user to reindex
+    'plugin_index_version'     => 5, // Update this value to prompt the user to reindex
     'user_index_version'       => 1,
     'is_indexing'              => false,
 
     // User settings
-    'enable_integration'       => true,
+    'integration_level'        => Config::INTEGRATION_LEVEL_FULL,
     'hosts'                    => '127.0.0.1:9200',
     'index_name'               => null,
     'shards'                   => 5,
